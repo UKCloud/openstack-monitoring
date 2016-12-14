40a8f4fe54d667bfd9fbe2c69e0bc9bcb210881a
Charles Llewellyn <charlie.j.llewellyn@gmail.com>
2016-12-14 17:13:13 +0000

    create and delete server

--- operational-tasks.py
+++ operational-tasks.py
@@ -17,6 +17,8 @@ For a full guide see TODO(etoews):link to docs on developer.openstack.org
 
 import argparse
 import os
+import time
+import json
 
 import os_client_config
 
@@ -31,14 +33,18 @@ utils.enable_logging(True, stream=sys.stdout)
 #: file, typically in $HOME/.config/openstack/clouds.yaml. That configuration
 #: will determine where the examples will be run and what resource defaults
 #: will be used to run the examples.
-TEST_CLOUD = os.getenv('OS_TEST_CLOUD', 'cor00005')
+CLOUD_NAME = 'cor00005'
+TEST_CLOUD = os.getenv('OS_TEST_CLOUD', CLOUD_NAME)
+SERVER_NAME = 'openstacksdk-example'
+IMAGE_NAME = 'CentOS 7'
+FLAVOR_NAME = 'm1.small'
+NETWORK_NAME = 'NetworkTest'
 
 class Opts(object):
-    def __init__(self, cloud_name='cor00005', debug=False):
+    def __init__(self, cloud_name=CLOUD_NAME, debug=False):
         self.cloud = cloud_name
-        self.debug = debug
-        # Use identity v3 API for examples.
-        self.identity_api_version = '2'
+        self.debug = False
+        self.identity_api_version = '3'
 
 def create_connection_from_config():
     opts = Opts(cloud_name=TEST_CLOUD)
@@ -46,31 +52,32 @@ def create_connection_from_config():
     cloud = occ.get_one_cloud(opts.cloud)
     return connection.from_config(cloud_config=cloud, options=opts)
 
-def list_servers(conn):
-    print("List Servers:")
+def create_server(conn):
+    print("Create Server:")
 
-    for server in conn.compute.servers():
-        print(server)
+    image = conn.compute.find_image(IMAGE_NAME)
+    flavor = conn.compute.find_flavor(FLAVOR_NAME)
+    network = conn.network.find_network(NETWORK_NAME)
+    results = {}
+
+    start_time = time.time()
+    server = conn.compute.create_server(
+        name=SERVER_NAME, image_id=image.id, flavor_id=flavor.id,
+        networks=[{"uuid": network.id}])
+    server = conn.compute.wait_for_server(server)
+
+    taskTime = time.time() - start_time
+
+    results['task_name'] = 'create server'
+    results['time'] = taskTime
+    print(json.dumps(results))
+    delete_server(conn, server)
 
 
-def list_images(conn):
-    print("List Images:")
+def delete_server(conn, server):
+    print("Delete Server:")
 
-    for image in conn.compute.images():
-        print(image)
+    conn.compute.delete_server(server)
 
-
-def list_flavors(conn):
-    print("List Flavors:")
-
-    for flavor in conn.compute.flavors():
-        print(flavor)
-
-
-def list_keypairs(conn):
-    print("List Keypairs:")
-
-    for keypair in conn.compute.keypairs():
-        print(keypair)
-
-list_images(create_connection_from_config())
+conn = create_connection_from_config()
+server = create_server(conn)
